c4d9b36c24955786f131141d7d11d1804f7f3ac6
local_adm <a-zakharov@bk.ru>
2026-07-21 23:07:56 +0300

    Basic_auth_2

--- app/components/navigate/topnav.vue
+++ app/components/navigate/topnav.vue
@@ -77,7 +77,7 @@ watch(
   adUser,
   (newUser) => {
     if (newUser) {
-      console.log('Сессия успешно считана и обновилась:', newUser);
+      // console.log('Сессия успешно считана и обновилась:', newUser);
       // @ts-ignore
       userDep.value = newUser.department || '';
       // @ts-ignore
