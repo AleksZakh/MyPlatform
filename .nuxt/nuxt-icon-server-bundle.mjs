dc5a177518b0eb55df4b8267607b5190638e109f
local_adm <a-zakharov@bk.ru>
2026-08-13 10:54:58 +0300

    firstStart

--- .nuxt/nuxt-icon-server-bundle.mjs
+++ .nuxt/nuxt-icon-server-bundle.mjs
@@ -12,6 +12,7 @@ function createRemoteCollection(fetchEndpoint) {
 }
 
 export const collections = {
+  'gravity-ui': () => require('@iconify-json/gravity-ui/icons.json'),
   'heroicons': () => require('@iconify-json/heroicons/icons.json'),
   'line-md': () => require('@iconify-json/line-md/icons.json'),
   'lucide': () => require('@iconify-json/lucide/icons.json'),
@@ -20,5 +21,10 @@ export const collections = {
   'ph': () => require('@iconify-json/ph/icons.json'),
   'simple-icons': () => require('@iconify-json/simple-icons/icons.json'),
   'solar': () => require('@iconify-json/solar/icons.json'),
+  'streamline-cyber-color': () => require('@iconify-json/streamline-cyber-color/icons.json'),
+  'streamline-flex-color': () => require('@iconify-json/streamline-flex-color/icons.json'),
   'streamline-freehand-color': () => require('@iconify-json/streamline-freehand-color/icons.json'),
+  'streamline-ultimate-color': () => require('@iconify-json/streamline-ultimate-color/icons.json'),
+  'system-uicons': () => require('@iconify-json/system-uicons/icons.json'),
+  'vscode-icons': () => require('@iconify-json/vscode-icons/icons.json'),
 }
--- app/components/lab/ExportRecordsModal.vue
+++ app/components/lab/ExportRecordsModal.vue
@@ -1,7 +1,7 @@
 <template>
     <UModal >
         <template #header>
-            <div class="flex items-center justify-between w-full">
+            <div class="flex items-center justify-between  w-full">
                 <div class="flex items-center gap-3">
                     <Icon name="vscode-icons:file-type-excel2" size="24"/>
                     <div>
@@ -20,7 +20,7 @@
         </template>
         <template #body>
             <div class="space-y-4 relative">
-                <div class="space-y-4 space-x-0 px-2 pb-3 -my-6 -mx-6 bg-gray-50 parent">
+                <div class="space-y-4 space-x-0 px-2 pb-3 bg-gray-50 parent">
                     <fieldset class="border-2 border-gray-200 mx-4  px-2 py-1 rounded-md bg-white/80">
                         <legend class="text-xl font-normal px-2 flex items-center gap-2 bg-transparent">
                             <span class="flex items-center gap-3">
